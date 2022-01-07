3f87467f67f559779921adba70de31b23df40b5a
KathriaN <kathrianmarques95@gmail.com>
2022-01-07 16:15:43 -0400

    primeiro commit

---
+++ ProjetoElevador/Model/Elevador.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoElevador.Model
+{
+    public class Elevador
+    {
+        public int qtdAndares, capElevador, qtdEspacos = 0, qtdPessoas = 0, andarAtual = 0;
+
+        public void Inicializar()
+        {
+            do
+            {
+                Console.WriteLine("Informe a quantidade de andares no prédio:");
+                qtdAndares = int.Parse(Console.ReadLine());
+
+                // TRATAMENTO DE ENTRADA DE VALOR INVÁLIDO
+                if (qtdAndares <= 0)
+                {
+                    Console.WriteLine("\nQUANTIDADE DE ANDARES INVÁLIDA! Informe um número inteiro positivo!\n");
+                }
+            }
+            while (qtdAndares <= 0);
+            do
+            {
+                Console.WriteLine("\nInforme a capacidade máxima do elevador:");
+                capElevador = int.Parse(Console.ReadLine());
+
+                // TRATAMENTO DE ENTRADA DE VALOR INVÁLIDO
+                if (capElevador <= 0)
+                {
+                    Console.WriteLine("\nCAPACIDADE INVÁLIDA! Informe um número inteiro positivo!");
+                }
+            }
+            while (capElevador <= 0);
+            Console.WriteLine("\nO ELEVADOR INICIA VAZIO NO TÉRREO!\n");
+        }
+
+        public void Entrar()
+        {
+            int qtdEspacos = capElevador - qtdPessoas;
+
+            if (qtdEspacos == 0) // VERIFICAR SE HÁ ESPAÇO NO ELEVADOR
+            {
+                Console.WriteLine("\nNÃO HÁ ESPAÇO NO ELEVADOR! ELEVADOR CHEIO!\n");
+            }
+            else
+            {
+                qtdPessoas += 1; // INCREMENTAR O NÚMERO DE PESSOAS NO ELEVADOR
+
+                Console.WriteLine("\nA quantidade atual de pessoas no elevador é: {0}", qtdPessoas);
+
+                //VERIFICAR EM QUAL ANDAR ESTÁ O ELEVADOR
+                if (andarAtual == 0)
+                {
+                    Console.WriteLine("O elevador está no térreo.\n");
+                }
+                else
+                {
+                    Console.WriteLine("O elevador está no {0}º Andar.\n", andarAtual);
+                }
+            }
+        }
+
+        public void Sair()
+        {
+            if (qtdPessoas == 0) // VERIFICAR SE HÁ PESSOAS NO ELEVADOR
+            {
+                Console.WriteLine("\nNÃO HÁ MAIS PESSOAS NO ELEVADOR! ELEVADOR VAZIO!\n");
+            }
+            else
+            {
+                qtdPessoas -= 1; // DECREMENTAR O NÚMERO DE PESSOAS NO ELEVADOR
+
+                Console.WriteLine("\nA quantidade atual de pessoas no elevador é: {0}", qtdPessoas);
+
+                //VERIFICAR EM QUAL ANDAR ESTÁ O ELEVADOR
+                if (andarAtual == 0)
+                {
+                    Console.WriteLine("O elevador está no térreo.\n");
+                }
+                else
+                {
+                    Console.WriteLine("O elevador está no {0}º Andar.\n", andarAtual);
+                }
+            }
+        }
+
+        public void Subir()
+        {
+            if (andarAtual == qtdAndares) // VERIFICAR SE O ELEVADOR ESTÁ NO ULTIMO ANDAR
+            {
+                Console.WriteLine("\nNÃO É POSSÍVEL MAIS SUBIR! JÁ ESTÁ NO ÚLTIMO ANDAR!\n");
+            }
+            else
+            {
+                andarAtual += 1; // SUBIR O ELEVADOR EM UM ANDAR
+
+                Console.WriteLine("\nA quantidade atual de pessoas no elevador é: {0}", qtdPessoas);
+
+                //VERIFICAR EM QUAL ANDAR ESTÁ O ELEVADOR
+                if (andarAtual == 0)
+                {
+                    Console.WriteLine("O elevador está no térreo.\n");
+                }
+                else
+                {
+                    Console.WriteLine("O elevador está no {0}º Andar.\n", andarAtual);
+                }
+            }
+        }
+
+        public void Descer()
+        {
+            if (andarAtual == 0) // VERIFICAR SE O ELEVADOR ESTÁ NO TÉRREO
+            {
+                Console.WriteLine("\nNÃO É POSSÍVEL MAIS DESCER! JÁ ESTÁ NO TÉRREO!\n");
+            }
+            else
+            {
+                andarAtual -= 1; // DESCER O ELEVADOR EM UM ANDAR
+                Console.WriteLine("\nA quantidade atual de pessoas no elevador é: {0}", qtdPessoas);
+
+                //VERIFICAR EM QUAL ANDAR ESTÁ O ELEVADOR
+                if (andarAtual == 0)
+                {
+                    Console.WriteLine("O elevador está no térreo.\n");
+                }
+                else
+                {
+                    Console.WriteLine("O elevador está no {0}º Andar.\n", andarAtual);
+                }
+            }
+        }
+    }
+}
---
+++ ProjetoElevador/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using ProjetoElevador.Model;
+
+namespace ProjetoElevador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SEJA BEM VINDO A APLICAÇÃO ELEVADOR!\n");
+            
+            //Instância da classe Elevador
+
+            Elevador elevador = new Elevador();
+            
+            //Chamada do método inicializar
+            elevador.Inicializar();
+
+            //Variável que define a continuação da aplicação
+            bool continuar = true;
+            
+            do
+            {
+                //Menu de Opções do Elevador
+                Console.WriteLine(@"    O que deseja fazer?
+                                    1 - Adicionar uma pessoa no elevador.
+                                    2 - Remover uma pessoa no elevador.
+                                    3 - Subir um andar.
+                                    4 - Descer um andar.
+                                    5 - Sair da Aplicação.
+
+                                    ");
+                string escolha = Console.ReadLine();
+                Console.Clear();
+
+                //Verificação da escolha do usuário
+                switch (escolha)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("\nVocê escolheu adicionar uma pessoa no elevador.\n");
+                            elevador.Entrar();
+                        }
+                        break;
+                    case "2":
+                        {
+                            Console.WriteLine("\nVocê escolheu retirar uma pessoa no elevador.\n");
+                            elevador.Sair();
+                        }
+                        break;
+                    case "3":
+                        {
+                            Console.WriteLine("\nVocê escolheu subir um andar no prédio.\n");
+                            elevador.Subir();
+                        }
+                        break;
+                    case "4":
+                        {
+                            Console.WriteLine("\nVocê escolheu descer um andar no prédio.\n");
+                            elevador.Descer();
+                        }
+                        break;
+                    case "5":
+                        {
+                            Console.WriteLine("\nVocê escolheu sair da aplicação.\n");
+                            continuar = false;
+                        }
+                        break;
+                    default:
+                        {
+                            Console.WriteLine("OPÇÃO INVÁLIDA!\n");
+                        }
+                        break;
+                }
+            }
+            while (continuar == true);
+
+        }
+    }
+}
